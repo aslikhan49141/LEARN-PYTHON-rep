a6811d94b2cdbb55e2c661e2ce04985ad44c41a7
aslikhan49141 <aslikhan49141@gmail.com>
2026-05-22 00:41:01 +0600

    statements

---
+++ statements.py
@@ -0,0 +1,14 @@
+a = 3333
+b = 200
+loggedin = False
+if b > a:
+    print("b is greater than a")
+    loggedin = True
+
+
+if not loggedin:
+    print("Please login to continue")
+else:
+    print("Welcome back!")
+
+

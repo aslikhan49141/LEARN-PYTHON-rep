318b2299764d87c0d687aa2e8a2b331ba4d9edda
aslikhan49141 <aslikhan49141@gmail.com>
2026-05-21 23:42:39 +0600

    Lening conditions

--- firstpycode.py
+++ firstpycode.py
@@ -8,3 +8,22 @@ time = 10
 cg = 3.44
 
 print(f"{greet}my age is {age} my cg is {cg}")
+x=[1,2,3,4,5]
+print(type(x))
+
+
+y = 'HelloZ'
+
+print(type(y))  
+for i in x :
+    print(i)
+
+    g=True
+if g:
+    print("g is true")
+
+    if cg>4 :
+        print ("you are a lier")
+    else :
+        print(f"great your cg is {cg}")
+    

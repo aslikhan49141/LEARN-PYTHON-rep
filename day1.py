1abe414c0afa43de32d8266c658e10a2916acf6d
aslikhan49141 <aslikhan49141@gmail.com>
2026-06-14 09:58:15 +0600

    operators

---
+++ day1.py
@@ -0,0 +1,73 @@
+print("Hey Alif Shahiriar Likhan")
+
+a=10 
+b=3
+print ("Addition = ", a+b)
+print ("Subtraction = ", a-b)
+print ("Multiplication = ", a*b)
+print ("Division = ", a/b)
+print ("Modulus = ", a%b)
+print ("Exponent = ", a**b)
+print ("Integer division = ", a//b)
+
+
+a= True
+b= False
+print("Adding boolean = ", a+b)
+print("Multiplying boolean = ", a*b)
+
+a= True
+b= True
+print("Adding boolean = ", a+b)
+
+a= "Hello"
+b= "World"
+print("concatenation ", a+" "+b)
+print("repetition ", a*3)
+
+a=10
+b=20
+c=10
+
+print("Average = ", (a+b+c) /3)
+
+#comparison operators
+a=10
+b=20
+print("a > b = ", a>b)
+print("a < b = ", a<b)
+print("a == b = ", a==b)
+print("a != b = ", a!=b)
+print("a >= b = ", a>=b)
+print("a <= b = ", a<=b)
+
+is_weekend =True
+is_holiday = False
+can_rest = is_weekend or is_holiday
+print("Can I rest? ", can_rest)
+
+has_id_card =True
+has_regestation = False
+can_enter = has_id_card and has_regestation
+print("Can I enter the university? ", can_enter)
+
+is_raining = False
+print("Is raining " ,not is_raining)
+print("Is raining " ,is_raining)
+
+
+
+a=10
+b=4
+print(a&b) #bitwise AND
+print(a|b) #bitwise OR  
+print(a^b) #bitwise XOR
+print(~a)  #bitwise NOT
+
+name = "Alif Shahiriar Likhan"
+print ("Hello,"+ name + " Welcome to Python programming!")
+print(f"Hello, {name} Welcome to Python programming!")
+
+price = 19.234567890234567
+print(f"The price is {price:.2f}")
+

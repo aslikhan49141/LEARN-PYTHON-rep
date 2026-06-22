5f0783b2244abce2b886f562f60c4b4d9e40a1b8
aslikhan49141 <aslikhan49141@gmail.com>
2026-06-23 01:02:22 +0600

    basic function

---
+++ functions.py
@@ -0,0 +1,12 @@
+# This file will contain function related codes
+def function_name(parameters):
+    # Function body
+    pass
+    # return value
+
+def add(a: int, b: int) : #-> int:
+    s=a+b
+    return s
+   # return a + b
+
+print(add(5, 3))
